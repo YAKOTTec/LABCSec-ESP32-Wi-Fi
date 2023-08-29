5180d3d110b0aa600e7866c6ea32654eba871675
YAKOTT <85380933+YAKOTTec@users.noreply.github.com>
2023-08-29 00:28:31 -0500

    Update HwM_E6.py

--- HwM_E6.py
+++ HwM_E6.py
@@ -10,7 +10,7 @@ Powered by YAKOTT
 
 """
 ESP32
-Wi-Fi MAC Flooding Attack
+Wi-Fi DHCP Starvation Attack
 
 https://docs.micropython.org/en/latest/library/network.html
 https://mpython.readthedocs.io/en/latest/library/micropython/network.html
@@ -43,7 +43,7 @@ wait_time_s = 1
 
 
 # Main
-print("Wi-Fi MAC Flooding Attack")
+print("Wi-Fi DHCP Starvation Attack")
 print()
 
 # WLAN() => Instantiate a network interface object
