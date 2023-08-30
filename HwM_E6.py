8417cb9498843b6e933cea09ed7cebc50a76d291
YAKOTT <85380933+YAKOTTec@users.noreply.github.com>
2023-08-30 00:00:31 -0500

    Update HwM_E6.py

--- HwM_E6.py
+++ HwM_E6.py
@@ -10,7 +10,7 @@ Powered by YAKOTT
 
 """
 ESP32
-Wi-Fi DHCP Starvation Attack
+WLAN DHCP Starvation Attack
 
 https://docs.micropython.org/en/latest/library/network.html
 https://mpython.readthedocs.io/en/latest/library/micropython/network.html
@@ -43,7 +43,7 @@ wait_time_s = 1
 
 
 # Main
-print("Wi-Fi DHCP Starvation Attack")
+print("WLAN DHCP Starvation Attack")
 print()
 
 # WLAN() => Instantiate a network interface object
